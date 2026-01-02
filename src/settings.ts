fd0132b7f60cd095cb58ab354a7cd6766526452c
liaoshunag <1773899029@qq.com>
2026-01-02 19:50:02 +0800

    chore: 更新项目名称为"新闻后台管理系统"
    统一修改开发环境、生产环境和设置文件中的项目名称，并简化登录页面标题显示

--- src/settings.ts
+++ src/settings.ts
@@ -7,7 +7,7 @@ const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
 
 export const defaultSettings: AppSettings = {
   // 系统Title
-  title: pkg.name,
+  title: "新闻后台管理系统",
   // 系统版本
   version: pkg.version,
   // 是否显示设置
